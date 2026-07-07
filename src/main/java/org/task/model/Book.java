e4e27618de85f7bf5aeedfd95adb478fcc18483a
vpopa <vpopa@griddynamics.com>
2026-07-07 15:11:37 +0300

    experiment with JPA entity lifecycle using EntityManager and Repository

--- src/main/java/org/task/model/Book.java
+++ src/main/java/org/task/model/Book.java
@@ -16,12 +16,14 @@ public class Book {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Setter
     private String title;
     private String author;
     private String genre;
     private String content;
     private double price;
 
+    @Setter
     @OneToMany(mappedBy = "book", cascade = CascadeType.ALL)
     private List<Review> reviews;
 }
