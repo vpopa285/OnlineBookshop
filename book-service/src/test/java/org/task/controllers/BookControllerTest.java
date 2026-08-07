dc5c294d2c1f56b2ca89078de2bb4f246ea7a3be
vpopa285 <vpopa@griddynamics.com>
2026-08-07 17:45:22 +0300

    unstaged file fix

--- book-service/src/test/java/org/task/controllers/BookControllerTest.java
+++ book-service/src/test/java/org/task/controllers/BookControllerTest.java
@@ -4,15 +4,18 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
-import org.task.BookshopApplication;
+import org.task.BookServiceApplication;
+import org.task.client.UserClient;
 
 import static org.hamcrest.Matchers.hasSize;
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.patch;
@@ -21,14 +24,17 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@SpringBootTest(classes = BookshopApplication.class)
+@SpringBootTest(classes = BookServiceApplication.class)
 @ActiveProfiles("test")
 @Sql(scripts = "/db/init.sql")
 class BookControllerTest {
     @Autowired
-    WebApplicationContext context;
+    private WebApplicationContext context;
 
-    MockMvc mockMvc;
+    @MockitoBean
+    private UserClient userClient;
+
+    private MockMvc mockMvc;
 
     @BeforeEach
     void setUp() {
@@ -58,7 +64,6 @@ class BookControllerTest {
                                 }
                                 """))
                 .andExpect(status().isCreated())
-                .andExpect(jsonPath("$.id").value(4))
                 .andExpect(jsonPath("$.title").value("Domain-Driven Design"));
 
         mockMvc.perform(put("/books/4")
@@ -87,6 +92,8 @@ class BookControllerTest {
 
     @Test
     void shouldRunBookReviewHappyPaths() throws Exception {
+        when(userClient.existsById(3L)).thenReturn(true);
+
         mockMvc.perform(get("/books/1/reviews"))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$", hasSize(2)));
