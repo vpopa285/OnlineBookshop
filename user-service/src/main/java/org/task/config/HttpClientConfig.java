83cfb357454dfae6c3fd00805ebf0002cf7f5aa5
vpopa285 <vpopa@griddynamics.com>
2026-08-12 18:17:20 +0300

    fix

--- user-service/src/main/java/org/task/config/HttpClientConfig.java
+++ user-service/src/main/java/org/task/config/HttpClientConfig.java
@@ -3,7 +3,6 @@ package org.task.config;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
 import org.springframework.web.client.RestClient;
 
 @Configuration
