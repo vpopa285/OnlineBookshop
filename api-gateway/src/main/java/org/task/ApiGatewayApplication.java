c295f0f7738bf71c5814fa955f636286fd7009c3
vpopa285 <vpopa@griddynamics.com>
2026-08-19 17:33:57 +0300

    api-gateway

---
+++ api-gateway/src/main/java/org/task/ApiGatewayApplication.java
@@ -0,0 +1,16 @@
+package org.task;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.hibernate.autoconfigure.HibernateJpaAutoConfiguration;
+import org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration;
+
+@SpringBootApplication(exclude = {
+        DataSourceAutoConfiguration.class,
+        HibernateJpaAutoConfiguration.class
+})
+public class ApiGatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ApiGatewayApplication.class, args);
+    }
+}
